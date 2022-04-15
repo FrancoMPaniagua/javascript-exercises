15f6c544f21db119b0592dc56ddc0a8525cff7c4
Franco Paniagua <francomanup3223@gmail.com>
2022-04-15 12:23:02 -0300

    did exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
 
+const reverseString = function(toReverse) {
+    let auxArray = toReverse.split('');
+    aLength = auxArray.length;
+    
+    for (i = 0;i < Math.floor(aLength/2);i++) {
+        let aux = auxArray[i];
+        auxArray[i] = auxArray[aLength - 1 - i];
+        auxArray[aLength - 1 - i] = aux;
+    }
+    return auxArray.join('');
 };
 
 // Do not edit below this line
