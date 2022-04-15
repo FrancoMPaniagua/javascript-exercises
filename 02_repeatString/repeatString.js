8995fe9e06cfaed3d44b3ab53873e40d4e24172d
FrancoMPaniagua <francomanup3223@gmail.com>
2022-04-15 11:14:51 -0300

    did exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(input,num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else {
+        let tempString = '';
+        for (i = 0;i < num;i++) {
+            tempString += input;
+        }
+        return tempString;
+    }
 };
 
 // Do not edit below this line
