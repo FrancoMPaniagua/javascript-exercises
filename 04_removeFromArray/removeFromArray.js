9d3a87226b4d88b8a68cd39c6aa6f21e1a9dd6cc
Franco Paniagua <francomanup3223@gmail.com>
2022-04-15 21:33:21 -0300

    did all but 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    const filteredArray = [];
+    array.forEach( (item) => {
+        if (!args.includes(item)){
+            filteredArray.push(item);
+        }
+    }
 
+    )
+    return filteredArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  newTemp =  (9 / 5) * (temp - 32);
+  return Math.round(newTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  newTemp = temp * (9 / 5) + 32;
+  return Math.round(newTemp * 10) / 10;
 };
 
 // Do not edit below this line
